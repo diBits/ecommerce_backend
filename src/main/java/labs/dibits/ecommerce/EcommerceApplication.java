d5250889a8ad9d7074ab68fa7281835133b123da
diBits <dibits.labs@gmail.com>
2023-09-07 16:15:47 -0300

    Paginacao com parametros para dominio categoria

--- src/main/java/labs/dibits/ecommerce/EcommerceApplication.java
+++ src/main/java/labs/dibits/ecommerce/EcommerceApplication.java
@@ -64,6 +64,14 @@ public class EcommerceApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Colecionáveis");
+		Categoria cat4 = new Categoria(null, "Vale Compras");
+		Categoria cat5 = new Categoria(null, "Decoração");
+		Categoria cat6 = new Categoria(null, "E-books");
+		Categoria cat7 = new Categoria(null, "DIY");
+		
+		
+		
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -91,7 +99,7 @@ public class EcommerceApplication implements CommandLineRunner {
 		
 		
 
-		categoriaRepostory.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepostory.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(e1,e2,e3));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
--- src/main/java/labs/dibits/ecommerce/services/CategoriaService.java
+++ src/main/java/labs/dibits/ecommerce/services/CategoriaService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import labs.dibits.ecommerce.domain.Categoria;
 import labs.dibits.ecommerce.repositories.CategoriaRepository;
@@ -56,6 +58,11 @@ public class CategoriaService {
 	}
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+	    PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
